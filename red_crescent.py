4f73e4b090758467ec8f69c9998d023badf0f03e
Intellectual-PSA <47997828+Intellectual-PSA@users.noreply.github.com>
2023-05-25 21:44:52 -0700

    Add files via upload

---
+++ red_crescent.py
@@ -0,0 +1,14 @@
+def display_neutral_red_crescent():
+    red_crescent = """
+    .-.
+   /   \
+   \   /
+    `-'
+    """
+
+    print(red_crescent)
+    print("\nThe Red Crescent is a neutral symbol and should not be used by any government or military force in a way that violates the Geneva Conventions.\n")
+
+# Sample usage
+if __name__ == "__main__":
+    display_neutral_red_crescent()
