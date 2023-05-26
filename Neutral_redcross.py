bee432b4592c702a42cbb2486d3c8a8b97524e9d
Intellectual-PSA <47997828+Intellectual-PSA@users.noreply.github.com>
2023-05-25 21:37:17 -0700

    Add files via upload
    When you run this program, it will print a representation of the Red Cross and a statement about its neutrality.
    
    Note: This is a very simplistic representation and does not truly demonstrate the gravity of the misuse of the Red Cross symbol or the importance of its neutrality as mandated by the Geneva Conventions.

---
+++ Neutral_redcross.py
@@ -0,0 +1,15 @@
+def display_neutral_red_cross():
+    red_cross = """
+    +-----+
+    |  X  |
+    |XXX |
+    |  X  |
+    +-----+
+    """
+
+    print(red_cross)
+    print("\nThe Red Cross is a neutral symbol and should not be used by any government or military force in a way that violates the Geneva Conventions.\n")
+
+# Sample usage
+if __name__ == "__main__":
+    display_neutral_red_cross()
